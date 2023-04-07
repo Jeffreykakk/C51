6810cd7c608831caf1ea28cd7dff30366d60214a
Jeffreykakk <1920466903@qq.com>
2023-04-07 13:23:19 +0800

    4-7

---
+++ 7.0数码管动态显示138译码器/main.c
@@ -0,0 +1,33 @@
+#include <REGX51.H>
+
+void Delay1ms(unsigned int xms)		
+{
+	unsigned char i, j;
+	while(xms)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+		xms--;
+	}
+	
+}
+
+unsigned char ch1[]={0x76,0x79,0x38,0x38,0x3F,0x00,0x6D,0x7F};
+unsigned char ch2[]={0x00,0x01,0X02,0x03,0x04,0x05,0x06,0x07};
+void main()
+{
+	while(1)
+	{
+		int i=0;
+		for(i=0;i<8;i++)
+		{
+			P3=ch2[i];
+			P2=ch1[i];
+			Delay1ms(5);
+		}
+	}
+}
