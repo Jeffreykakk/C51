9c2ef0126a84eedf769215f235c1f91e2e887d00
Jeffreykakk <1920466903@qq.com>
2023-04-08 21:11:17 +0800

    4-8

---
+++ 12.0两位数码管静态显示/main.c
@@ -0,0 +1,34 @@
+#include <REGX51.H>
+unsigned char duanma[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71};
+sbit L1=P2^1;
+sbit L0=P2^0;
+
+void delay()
+{
+	unsigned int i,j;
+	for(i=0;i<100;i++);
+	for(j=0;j<10;j++);
+}
+
+void shuzi(unsigned int x)
+{
+		P1=0x00;
+		L1=0;
+		L0=1;
+		P1=duanma[x%10];
+		delay();
+			
+		L1=1;
+		L0=0;
+		P1=duanma[x/10];
+		delay();
+}
+
+void main()
+{
+	while(1)
+		shuzi(10); 
+}
+
+
+
